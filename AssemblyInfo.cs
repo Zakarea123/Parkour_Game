c94d38e45a8982d3370c9e855408d2d675385433
Zakarea123 <zakreaalamor12@gmail.com>
2025-10-29 21:46:43 +0100

    Add Comments

--- AssemblyInfo.cs
+++
@@ -1,3 +0,0 @@
-using System.Runtime.CompilerServices;
-// Allow UnitTests to access internal Variables 
-[assembly: InternalsVisibleTo("GameTesting")]
--- World/KillZone.cs
+++ World/KillZone.cs
@@ -7,6 +7,7 @@ public partial class KillZone : Area2D
     
     public async void _on_body_entered(Node body)
     {
+        // Remove 1 heart when colliding with the Kill zone
         if (body is Player player)
         {
            await GameManager.Instance.DamagePlayer();
