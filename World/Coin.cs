df5018accfe05de0b8714864c8eac5dfda836e52
Zakarea123 <zakreaalamor12@gmail.com>
2025-11-01 21:07:39 +0100

    Added Music and SFX

--- World/Coin.cs
+++ World/Coin.cs
@@ -5,18 +5,20 @@ public partial class Coin : Area2D
 
 {
 	private GameManager gameManager;
+    private AnimationPlayer animationPlayer;
 	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		BodyEntered += OnBodyEntered;
+		animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
 	}
 	public void OnBodyEntered(Node body)
 	{
 		if (body is Player)
 		{
 			GameManager.Instance?.AddScore(1);
-			QueueFree();
+			animationPlayer.Play("pickup");
 		}
 		
 	}
